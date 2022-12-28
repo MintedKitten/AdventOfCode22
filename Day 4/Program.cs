1862816ec0a66321ed2eb1e10f4e9b5ad431223b
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2022-12-28 13:38:45 +0700

    Start of Day 4 Rename Files

---
+++ Day 4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+StreamReader reader = new("../../../../Input/input_d4.txt");
+
+string? line = "";
+while ((line = reader.ReadLine()) != null)
+{
+    line = line.Trim();
+    string[] assignments = line.Split(',');
+    string[][] arr = assignments.Select((arrs) => { return arrs.Split("-"); }).ToArray();
+
+    foreach (string[] _arr in arr)
+    {
+        Console.WriteLine($"{_arr[0]} {_arr[1]}");
+    }
+    Console.WriteLine($"====");
+}
