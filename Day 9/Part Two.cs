acdeaed953201679b6fcc80c4b04d4a0174c6297
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-02 00:21:16 +0700

    Day 9, Both Stars! It works and that's good enough

---
+++ Day 9/Part Two.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    internal class Part_Two
+    {
+        public static void Main()
+        {
+            using StreamReader reader = new("../../../../Input/input_d9.txt");
+
+            string? line = "";
+            RopeKnots ropes = new(10);
+            while ((line = reader.ReadLine()) != null)
+            {
+                line = line.Trim();
+                string[] movement = line.Split(' ');
+                switch (movement[0])
+                {
+                    case "L":
+                        for (int i = 0; i < int.Parse(movement[1]); i++)
+                        {
+                            ropes.MoveHeadLeft();
+                        }
+                        break;
+                    case "R":
+                        for (int i = 0; i < int.Parse(movement[1]); i++)
+                        {
+                            ropes.MoveHeadRight();
+                        }
+                        break;
+                    case "U":
+                        for (int i = 0; i < int.Parse(movement[1]); i++)
+                        {
+                            ropes.MoveHeadUp();
+                        }
+                        break;
+                    case "D":
+                        for (int i = 0; i < int.Parse(movement[1]); i++)
+                        {
+                            ropes.MoveHeadDown();
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine($"A Unique Tail History: {ropes.TailHistory.Count}");
+        }
+
+        class RopeKnots
+        {
+            public int[][] Knots { get; }
+            public HashSet<string> TailHistory { get; } = new() { string.Join("-", new int[] { 0, 0 }) };
+
+            public RopeKnots(int amount)
+            {
+                Knots = new int[amount][];
+                for (int i = 0; i < Knots.Length; i++)
+                {
+                    Knots[i] = new int[2] { 0, 0 };
+                }
+            }
+            public string KnotsPos()
+            {
+                return string.Join(" ", Knots.Select((knot, index) => { return $"[{knot[0]},{knot[1]}]"; }));
+            }
+            public void UpdateKnots()
+            {
+                for (int i = 0; i < Knots.Length; i++)
+                {
+                    if (i + 1 == Knots.Length)
+                    {
+                        string temp = string.Join("-", new int[] { Knots[i][0], Knots[i][1] });
+                        TailHistory.Add(temp);
+                        break;
+                    }
+                    if (TailNeedsToMove(Knots[i], Knots[i + 1], out int dx, out int dy))
+                    {
+                        Knots[i + 1][0] = Knots[i + 1][0] + dx;
+                        Knots[i + 1][1] = Knots[i + 1][1] + dy;
+                    }
+                }
+            }
+            public void MoveHeadUp()
+            {
+                Knots[0][1]++;
+                UpdateKnots();
+            }
+            public void MoveHeadDown()
+            {
+                Knots[0][1]--;
+                UpdateKnots();
+            }
+            public void MoveHeadLeft()
+            {
+                Knots[0][0]--;
+                UpdateKnots();
+            }
+            public void MoveHeadRight()
+            {
+                Knots[0][0]++;
+                UpdateKnots();
+            }
+            public static bool TailNeedsToMove(int[] front, int[] back, out int dx, out int dy)
+            {
+                if (front.Length != 2)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(front), "Array length should be 2");
+                }
+                if (back.Length != 2)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(back), "Array length should be 2");
+                }
+                dx = Math.Sign(front[0] - back[0]);
+                dy = Math.Sign(front[1] - back[1]);
+                if (Math.Abs(front[0] - back[0]) == 2)
+                {
+                    return true;
+                }
+                if (Math.Abs(front[1] - back[1]) == 2)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Day 9/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day_9;
+
+Part_Two.Main();
+Environment.Exit(0);
+
+using StreamReader reader = new("../../../../Input/input_d9.txt");
+
+string? line = "";
+Rope rp = new();
+while ((line = reader.ReadLine()) != null)
+{
+    line = line.Trim();
+    string[] movement = line.Split(' ');
+    switch (movement[0])
+    {
+        case "L":
+            for (int i = 0; i < int.Parse(movement[1]); i++)
+            {
+                rp.MoveHeadLeft();
+            }
+            break;
+        case "R":
+            for (int i = 0; i < int.Parse(movement[1]); i++)
+            {
+                rp.MoveHeadRight();
+            }
+            break;
+        case "U":
+            for (int i = 0; i < int.Parse(movement[1]); i++)
+            {
+                rp.MoveHeadUp();
+            }
+            break;
+        case "D":
+            for (int i = 0; i < int.Parse(movement[1]); i++)
+            {
+                rp.MoveHeadDown();
+            }
+            break;
+    }
+}
+Console.WriteLine($"End Unique Tail History: {rp.TailHistory.Count}");
+
+class Rope
+{
+    public int[] Head { get; } = { 0, 0 };
+    public int[] Tail { get; } = { 0, 0 };
+    public HashSet<string> TailHistory { get; } = new() { string.Join("-", new int[] { 0, 0 }) };
+
+    public Rope()
+    {
+    }
+    public void MoveHeadUp()
+    {
+        Head[1]++;
+        if (TailNeedsToMove())
+        {
+            Tail[0] = Head[0];
+            Tail[1] = Head[1] - 1;
+            string temp = string.Join("-", new int[] { Tail[0], Tail[1] });
+            TailHistory.Add(temp);
+        }
+    }
+    public void MoveHeadDown()
+    {
+        Head[1]--;
+        if (TailNeedsToMove())
+        {
+            Tail[0] = Head[0];
+            Tail[1] = Head[1] + 1;
+            string temp = string.Join("-", new int[] { Tail[0], Tail[1] });
+            TailHistory.Add(temp);
+        }
+    }
+    public void MoveHeadLeft()
+    {
+        Head[0]--;
+        if (TailNeedsToMove())
+        {
+            Tail[0] = Head[0] + 1;
+            Tail[1] = Head[1];
+            string temp = string.Join("-", new int[] { Tail[0], Tail[1] });
+            TailHistory.Add(temp);
+        }
+    }
+    public void MoveHeadRight()
+    {
+        Head[0]++;
+        if (TailNeedsToMove())
+        {
+            Tail[0] = Head[0] - 1;
+            Tail[1] = Head[1];
+            string temp = string.Join("-", new int[] { Tail[0], Tail[1] });
+            TailHistory.Add(temp);
+        }
+    }
+    public bool TailNeedsToMove()
+    {
+        if (Math.Abs(Head[0] - Tail[0]) == 2)
+        {
+            return true;
+        }
+        if (Math.Abs(Head[1] - Tail[1]) == 2)
+        {
+            return true;
+        }
+        return false;
+    }
+}
