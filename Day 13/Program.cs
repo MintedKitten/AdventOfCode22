3a7cc98da0513cf3012fd88cc62fa68e56e13602
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-10 04:43:54 +0700

    Day 13, start parsing data

---
+++ Day 13/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+using StreamReader reader = new("../../../../Input/input_d13t.txt");
+
+List<IList> first = new();
+List<IList> second = new();
+string? line = "";
+while ((line = reader.ReadLine()) != null)
+{
+    string line1 = line.Trim() + ",";
+    string line2 = reader.ReadLine()?.Trim() + "," ?? "";
+    string _ = reader.ReadLine()?.Trim() ?? "";
+
+    string blockS = "";
+    for (int i = 0; i < line1.Length; i++)
+    {
+        string tmp = line1[i].ToString();
+        if (tmp == ",")
+        {
+            for (int j = 0; j < blockS.Length; j++)
+            {
+                string sectionS = blockS[j].ToString();
+                if (sectionS == "[")
+                {
+                    Console.Write("SAr ");
+                }
+                else if (int.TryParse(sectionS, out int rc))
+                {
+                    Console.Write($"N:{rc} ");
+                }
+                else if (sectionS == "]")
+                {
+                    Console.Write("EAr ");
+                }
+            }
+            blockS = "";
+        }
+        else
+        {
+            blockS += tmp;
+        }
+    }
+    Console.WriteLine();
+    blockS = "";
+    for (int i = 0; i < line2.Length; i++)
+    {
+        string tmp = line2[i].ToString();
+        if (tmp == ",")
+        {
+            for (int j = 0; j < blockS.Length; j++)
+            {
+                string sectionS = blockS[j].ToString();
+                if (sectionS == "[")
+                {
+                    Console.Write("SAr ");
+                }
+                else if (int.TryParse(sectionS, out int rc))
+                {
+                    Console.Write($"N:{rc} ");
+                }
+                else if (sectionS == "]")
+                {
+                    Console.Write("EAr ");
+                }
+            }
+            blockS = "";
+        }
+        else
+        {
+            blockS += tmp;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
