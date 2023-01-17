7f8c9db4dfd91e5724fa305cc1230c811cddd28e
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-18 05:33:50 +0700

    Back into the groove after some really busy weeks

--- Day 13/Program.cs
+++ Day 13/Program.cs
@@ -1,77 +1,110 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Collections;
-
 using StreamReader reader = new("../../../../Input/input_d13t.txt");
 
-List<IList> first = new();
-List<IList> second = new();
 string? line = "";
+int round = 1;
+int total = 0;
 while ((line = reader.ReadLine()) != null)
 {
-    string line1 = line.Trim() + ",";
-    string line2 = reader.ReadLine()?.Trim() + "," ?? "";
+    string line1 = line.Trim();
+    string line2 = reader.ReadLine()?.Trim() ?? "";
     string _ = reader.ReadLine()?.Trim() ?? "";
 
-    string blockS = "";
-    for (int i = 0; i < line1.Length; i++)
+    Console.WriteLine($"Round: {round}");
+    List<object> left = GenNewDistressSigList(line1[1..], out _);
+    List<object> right = GenNewDistressSigList(line2[1..], out _);
+    writeList(left);
+    Console.WriteLine();
+    writeList(right);
+    Console.WriteLine();
+    Console.WriteLine();
+    round++;
+}
+
+//static bool compareLeftToRight(List<object> left, List<object> right)
+//{
+//    for (int i = 0; i < Math.Max(left.Count, right.Count); i++)
+//    {
+//        if (i == left.Count)
+//        {
+//            return true;
+//        }
+//        else if (i == right.Count)
+//        {
+//            return false;
+//        }
+//        else
+//        {
+//            if (left[i].GetType() == typeof(int))
+//            {
+               
+//            }
+//            continue;
+//        }
+//    }
+//    return false;
+//}
+
+static void writeList(List<object> lsob)
+{
+    Console.Write("[");
+    foreach (var item in lsob)
     {
-        string tmp = line1[i].ToString();
-        if (tmp == ",")
+        if (item.GetType() == typeof(int))
         {
-            for (int j = 0; j < blockS.Length; j++)
-            {
-                string sectionS = blockS[j].ToString();
-                if (sectionS == "[")
-                {
-                    Console.Write("SAr ");
-                }
-                else if (int.TryParse(sectionS, out int rc))
-                {
-                    Console.Write($"N:{rc} ");
-                }
-                else if (sectionS == "]")
-                {
-                    Console.Write("EAr ");
-                }
-            }
-            blockS = "";
+            Console.Write($"{item}-");
+        }
+        else if (item.GetType() == typeof(List<object>))
+        {
+            writeList((List<object>)item);
+        }
+    }
+    Console.Write("]");
+}
+
+static List<object> GenNewDistressSigList(string lines, out string remain)
+{
+    List<object> inner_list = new();
+    while (true)
+    {
+        if (lines[0].ToString() == "[")
+        {
+            inner_list.Add(GenNewDistressSigList(lines[1..], out string rm));
+            lines = rm;
+            continue;
         }
         else
         {
-            blockS += tmp;
-        }
-    }
-    Console.WriteLine();
-    blockS = "";
-    for (int i = 0; i < line2.Length; i++)
-    {
-        string tmp = line2[i].ToString();
-        if (tmp == ",")
-        {
-            for (int j = 0; j < blockS.Length; j++)
-            {
-                string sectionS = blockS[j].ToString();
-                if (sectionS == "[")
-                {
-                    Console.Write("SAr ");
-                }
-                else if (int.TryParse(sectionS, out int rc))
-                {
-                    Console.Write($"N:{rc} ");
-                }
-                else if (sectionS == "]")
-                {
-                    Console.Write("EAr ");
-                }
+            if (lines[0].ToString() == ",")
+            {   // List inside a list
+                lines = lines[1..];
+                continue;
             }
-            blockS = "";
-        }
-        else
-        {
-            blockS += tmp;
+            else if (lines[0].ToString() == "]")
+            {   // List is empty
+                remain = lines[1..];
+                return inner_list;
+            }
+            else
+            {
+                int potentialNextIndex = lines.IndexOf(",");
+                if (potentialNextIndex >= 0 && int.TryParse(lines[..potentialNextIndex], out int num))
+                {   // Number is not last in list, add number, then go to the next one
+                    inner_list.Add(num);
+                    lines = lines[potentialNextIndex..];
+                    continue;
+                }
+                else
+                {   // Last number of the list
+                    int endOfList = lines.IndexOf("]");
+                    num = int.Parse(lines[..endOfList]);
+                    inner_list.Add(num);
+                    lines = lines[endOfList..];
+                    continue;
+                }
+                // Once a number is added to the list, truncated the list to the next section
+            }
         }
     }
-    Console.WriteLine();
-    Console.WriteLine();
 }
